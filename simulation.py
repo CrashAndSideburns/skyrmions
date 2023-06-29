Fix errors in GinzburgLandau energy computation.
This commit fixes some errors with the energy computation for the GizburgLandau FixedPointMethod. The fix related to the meshgrid is straightforward, and only impacts the curl term. The change in the definition of λ2 is substantially less well-motivated, except with the goal of parity with the MATLAB implementation in mind. The previous implementation came from the code supplied in Appendix A of "Topological solitons in two-dimensional chiral magnets", in particular the case where τ = 1. The current definition comes from the code provided to me by the author of the original paper, but these definitions do not agree, even in the special case θ = π/2. It is not clear to me which definition of λ2 is actually correct, so I have gone for parity with the provided MATLAB code, as it appears to produce the figures contained in the paper once the matrix inversion algorithm is patched.

simulation.py
@@ -8,8 +8,8 @@ class Simulation:
         self,
         N,
         fixed_point_method,
-        energy_gradient_threshold=1e-6,
-        Δt_threshold=1e-4,
+        energy_gradient_threshold=1e-5,
+        Δt_threshold=1e-5,
     ):
         # Invariant: N must be odd.
         assert N % 2 == 1, "N must be odd."
@@ -99,7 +99,7 @@ class GinzburgLandau(FixedPointMethod):
         self.tolerance = tolerance
 
         # Compute bifurcation point.
-        λ2 = -α * (5 + np.cos(θ) ** 2) / 4
+        λ2 = -α
         λ0 = -1 - β / 2 + np.sqrt(4 * κ**2 + β**2 / 4)
         λ = λ0 + 0.01 * λ2
 
@@ -107,7 +107,7 @@ class GinzburgLandau(FixedPointMethod):
         # wave vectors associated with the 2D FFT of the magnetization field.
         # The somewhat strange order of the wave vectors is defined by np.fft2.
         k = np.array(list(range(int((N + 1) / 2))) + list(range(int(-(N - 1) / 2), 0)))
-        x, y = np.meshgrid(k, k)
+        y, x = np.meshgrid(k, k)
         y = -x / np.tan(θ) + y / (τ * np.sin(θ))
         norm_squared = x**2 + y**2
 